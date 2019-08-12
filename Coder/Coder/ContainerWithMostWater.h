98ac117b9f65a1ac280cc514361d510de644d390
fifamvp <fifamvp@icloud.com>
2019-08-12 17:17:48 +0800

    make it shorter, but runtime seems not be shorter

--- Coder/Coder/ContainerWithMostWater.h
+++ Coder/Coder/ContainerWithMostWater.h
@@ -24,43 +24,57 @@ public:
 
 private:
 
+	//int maxArea(vector<int>& height) {
+	//	//auto start_time = std::chrono::system_clock::now();
+
+	//	int result = 0;
+
+	//	for (size_t i = 0; i < height.size(); i++)
+	//	{
+	//		int left = 0;
+	//		int right = height.size() - 1;
+	//		while (left < i || right > i)
+	//		{
+	//			if (left < i && height[left] >= height[i])
+	//			{
+	//				int v = (i - left) * height[i];
+	//				if (v > result)
+	//					result = v;
+	//				break;
+	//			}
+	//			if (right > i && height[right] >= height[i])
+	//			{
+	//				int v = (right - i) * height[i];
+	//				if (v > result)
+	//					result = v;
+	//				break;
+	//			}
+	//			if (left < i)
+	//				left++;
+	//			if (right > i)
+	//				right--;
+	//		}
+	//	}
+
+	//	//auto end_time = std::chrono::system_clock::now();
+	//	//auto inteval = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+
+	//	//printf("%f\n", inteval);
+
+	//	return result;
+	//}
+
 	int maxArea(vector<int>& height) {
-		//auto start_time = std::chrono::system_clock::now();
-
 		int result = 0;
-
-		for (size_t i = 0; i < height.size(); i++)
+		int i = 0, j = height.size() - 1, h = 0;
+		while (i < j) 
 		{
-			int left = 0;
-			int right = height.size() - 1;
-			while (left < i || right > i)
-			{
-				if (left < i && height[left] >= height[i])
-				{
-					int v = (i - left) * height[i];
-					if (v > result)
-						result = v;
-					break;
-				}
-				if (right > i && height[right] >= height[i])
-				{
-					int v = (right - i) * height[i];
-					if (v > result)
-						result = v;
-					break;
-				}
-				if (left < i)
-					left++;
-				if (right > i)
-					right--;
-			}
+			h = min(height[i], height[j]);
+			result = max(result, (j - i) * h);
+
+			while (height[i] <= h && i < j)i++;
+			while (height[j] <= h && i < j)j--;
 		}
-
-		//auto end_time = std::chrono::system_clock::now();
-		//auto inteval = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
-
-		//printf("%f\n", inteval);
-
 		return result;
 	}
 };
