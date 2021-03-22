f06fdaa278c0070715117932ee0adad987def9b7
fifamvp <fifamvp@icloud.com>
2021-03-22 23:32:00 +0800

    Corret the function name in BinaryTreeMaximumPathSum.h

--- Coder/Coder/Solutions/Tree/BinaryTreeMaximumPathSum.h
+++ Coder/Coder/Solutions/Tree/BinaryTreeMaximumPathSum.h
@@ -11,26 +11,26 @@ public:
 private:
 	int maxSum = INT_MIN;
 	int maxPathSum(TreeNode* root) {
-		inorderTraversal(root);
+		getMaxLinked(root);
 		return maxSum;
 	}
 
-	int inorderTraversal(TreeNode* root)
+	int getMaxLinked(TreeNode* root)
 	{
 		if (!root) return 0;
-		int maxLinkedL = inorderTraversal(root->left);
-		int maxLinkedR = inorderTraversal(root->right);
+		int maxLinkedL = getMaxLinked(root->left);
+		int maxLinkedR = getMaxLinked(root->right);
 		int maxLinked = max(max(root->val + maxLinkedL, root->val + maxLinkedR), root->val);
 		maxSum = max(max(maxSum, root->val + maxLinkedL + maxLinkedR), maxLinked);
 		return maxLinked;
 	}
 
 	// 20 ms 27.5 MB
-	int inorderTraversal2(TreeNode* root)
+	int getMaxLinked2(TreeNode* root)
 	{
 		if (!root) return 0;
-		int maxLinkedL = max(inorderTraversal2(root->left), 0);
-		int maxLinkedR = max(inorderTraversal2(root->right), 0);
+		int maxLinkedL = max(getMaxLinked2(root->left), 0);
+		int maxLinkedR = max(getMaxLinked2(root->right), 0);
 		maxSum = max(maxSum, root->val + maxLinkedL + maxLinkedR);
 		return root->val + max(maxLinkedL, maxLinkedR);
 	}
