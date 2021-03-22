99ee67772c21eee9acccc1d6331e5928bedc02ed
fifamvp <fifamvp@icloud.com>
2021-03-22 23:31:26 +0800

    Binary Tree Inorder Traversal

--- Coder/Coder/Solutions/Tree/BinaryTreeInorderTraversal.h
+++ Coder/Coder/Solutions/Tree/BinaryTreeInorderTraversal.h
@@ -11,13 +11,13 @@ public:
 
 private:
 	// 4 ms	10.4 MB
-	vector<int> inorderTraversal(TreeNode* root) {
+	vector<int> getMaxLinked(TreeNode* root) {
 		vector<int> result;
 		if (!root) return result;
-		vector<int> left = inorderTraversal(root->left);
+		vector<int> left = getMaxLinked(root->left);
 		result.insert(result.end(), left.begin(), left.end());
 		result.push_back(root->val);
-		vector<int> right = inorderTraversal(root->right);
+		vector<int> right = getMaxLinked(root->right);
 		result.insert(result.end(), right.begin(), right.end());
 		return result;
 	}
